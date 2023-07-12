96000562eb67f1b201a265100c894833f3bbde9d
Roberto Modenezi Neto <futspamslt@gmail.com>
2023-07-11 22:33:39 -0300

    Added Update Post

---
+++ src/modules/posts/useCases/updatePost/updatePostController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { IRequestUpdatePost } from "@modules/posts/dtos/posts";
+import { container } from "tsyringe";
+import { UpdatePostUseCase } from "./updatePostUseCase";
+
+class UpdatePostController {
+  async handle(req: Request, res: Response) {
+    const { usrId } = req;
+
+    const { id } = req.params as { id: string };
+    const { content, tags, visibility } = req.body as IRequestUpdatePost;
+
+    const updatePostUseCase = container.resolve(UpdatePostUseCase);
+
+    const result = await updatePostUseCase.execute({
+      usrId,
+      id,
+      content,
+      tags,
+      visibility,
+    });
+
+    return res.status(result.statusCode).json(result);
+  }
+}
+
+export { UpdatePostController };
