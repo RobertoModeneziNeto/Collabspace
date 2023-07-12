c29fc618c687e5203ba0e7f7b8f7c746c2fb346b
Roberto Modenezi Neto <futspamslt@gmail.com>
2023-07-12 20:07:31 -0300

    Added total on posts search

--- src/modules/posts/iRepositories/iPostsRepositories.ts
+++ src/modules/posts/iRepositories/iPostsRepositories.ts
@@ -3,6 +3,7 @@ import { ICreatePost, IListAllPosts, IPost, IUpdatePost } from "../dtos/posts";
 interface IPostsRepositories {
   create(post: ICreatePost): Promise<IPost>;
   listAll(page: number, limit: number): Promise<IListAllPosts[]>;
+  count(): Promise<number>;
   listById(id: string): Promise<IPost | null>;
   update(data: IUpdatePost): Promise<void>;
 }
