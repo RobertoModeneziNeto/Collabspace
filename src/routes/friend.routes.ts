f38d0b0b0fcc89d1fc3c7806dffda4d08d6857d5
Roberto Modenezi Neto <futspamslt@gmail.com>
2023-07-21 22:35:20 -0300

    Added Cancel Request

--- src/routes/friend.routes.ts
+++ src/routes/friend.routes.ts
@@ -1,3 +1,4 @@
+import { CancelRequestController } from "@modules/friends/useCases/cancelRequest/cancelRequestController";
 import { CreateFriendController } from "@modules/friends/useCases/createFriend/createFriendController";
 import { Router } from "express";
 import { authentication } from "src/middlewares/authentication";
@@ -7,5 +8,6 @@ const friendRoutes = Router();
 friendRoutes.use(authentication);
 
 friendRoutes.post("/:targetId", new CreateFriendController().handle);
+friendRoutes.patch("/cancelRequest/:id", new CancelRequestController().handle);
 
 export { friendRoutes };
