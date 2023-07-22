c18674725e77b31b63dc51c4ffb70f55215d4b61
Roberto Modenezi Neto <futspamslt@gmail.com>
2023-07-21 21:39:08 -0300

    Added createFriend

---
+++ src/enums/reactionTypes.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+export enum ReactionTypes {
+  liked = 1, // Gostuei
+  loved = 2, // Não gostuei, asmei
+  haha = 3, // Haha
+  angry = 4, // Grr
+  sad = 5, // Truaisti
+  notKindding = 6, // la ele
+}
